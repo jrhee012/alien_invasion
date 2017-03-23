ecfd215ee19a5768c786f4658476b2b1d523da91
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2017-03-23 00:05:43 -0400

    escape key added

--- game_functions.py
+++ game_functions.py
@@ -11,6 +11,8 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings,screen, ship, bullets)
+    elif event.key == pygame.K_ESCAPE:
+        sys.exit()
 
 def fire_bullet(ai_settings, screen, ship, bullets):
     if len(bullets) < ai_settings.bullets_allowed:
